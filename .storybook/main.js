0cc3de79d0e7a62c606cecd516a167074b12d19e
Arpit Dubey <arpit@gamezop.co>
2021-08-28 12:12:44 +0000

    refactor(platform): added css module support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,35 @@
+const path = require('path')
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.@(ts|tsx)', '../src/**/*.stories.mdx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-dark-mode',
+    'storybook-addon-designs',
+    '@storybook/addon-a11y',
+    'storybook-css-modules-preset',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "core": {
-    "builder": "webpack5"
-  }
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@kit/components': path.resolve(__dirname, '../src/components'),
+      '@kit/containers': path.resolve(__dirname, '../src/containers'),
+      '@kit/pages': path.resolve(__dirname, '../src/pages'),
+      '@kit/styles': path.resolve(__dirname, '../src/styles'),
+      '@kit/utils': path.resolve(__dirname, '../src/utils'),
+    }
+
+    return config
+  },
+  core: {
+    builder: 'webpack5',
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,14 @@
 import '../src/styles/globals.css'
 
+import {themes} from '@storybook/theming'
+
 export const parameters = {
+  darkMode: {
+    // Override the default dark theme
+    dark: {...themes.dark, appBg: 'black'},
+    // Override the default light theme
+    light: {...themes.normal, appBg: 'white'},
+  },
   actions: {argTypesRegex: '^on[A-Z].*'},
   controls: {
     matchers: {
